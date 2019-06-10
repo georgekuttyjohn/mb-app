f5ecbc0d3f182ab0749a8a162329c119fae11ef0
drgeorge <drgeorge@corbelbiz.com>
2019-06-10 18:13:09 +0530

    Heroko

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b3jv=-*bd^e*g8lt#fyq^9srn4w*qjomg&+rn2_+fhzlf(4guq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

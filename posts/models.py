366e43d30cbe3b98e2ff6e6c41eed452dd29bbe9
drgeorge <drgeorge@corbelbiz.com>
2019-06-10 17:55:03 +0530

    added test

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
-    author = models.TextField(blank=True)
 
     def __str__(self):
         return self.text[:50]
